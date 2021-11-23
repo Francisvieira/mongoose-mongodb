3e2407b4c418d8eb807d439893bf167dbb919179
francisvieira <fvieira340@gmail.com>
2021-11-23 09:44:33 -0300

    codificao do crud

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 // Vamos construir um servidor usando o modulo do express.
 // este modulo possue funções para executar e manipular um servidor node.
 // iniciaremos criando uma referencia do express comn a importaçao de modulo 
-const express = require("express"); 
+const express = require("express");
 
 
 // vamos Importar o modulo mangoose que fara a interface entre o 
 //nodejs eo banco de dados mongodb 
-const mongoose =require("mongoose"); 
+const mongoose = require("mongoose");
 
 
 
- const url= "mongodb+srv://francisco:<franc1991>@clustercliente.pxsw7.mongodb.net/primeiraapi?retryWrites=true&w=majority"
- mongoose.connect(uri,{ useNewURLParser: true, useUnifiedTopology:true })
+const url = "mongodb+srv://francisco:franc1991@clustercliente.pxsw7.mongodb.net/primeiraapi?retryWrites=true&w=majority"
+mongoose.connect(url, { useNewURLParser: true, useUnifiedTopology: true })
 
 // Vamos criar a estrutura da tabela com o comando schema 
 
 const tabela = mongoose.Schema({
-    nome:{type:String, required:true},
-    email:{type:String,required:true,unique:true},
-    cpf:{type:String,required:true,unique:true},
-    usuario:{type:String,required:true,unique:true},
-    sennha:{type:String,required:true,}
+    nome: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    cpf: { type: String, required: true, unique: true },
+    usuario: { type: String, required: true, unique: true },
+    senha: { type: String, required: true, }
 });
 
 
-    // execucao da tabela
+// execucao da tabela
 
-    const Cliente = mongoose.model("tbcliente",tabela);
+const Cliente = mongoose.model("tbcliente", tabela);
 
 
 
@@ -34,10 +34,10 @@ const tabela = mongoose.Schema({
 
 // criar uma referencia do servidor express para utiliza-lo
 
-const app =express(); 
+const app = express();
 
 // fazer o servidor express receber e tratar dados em formato json
-app.use(express.json()); 
+app.use(express.json());
 /*
 Abaixo iremos as 4 rotas para o verbos GET,POST,PUT,DELETE:
 - GET -> Esse verbo é utilizado todas as vezes  que o usuario requisita alguma informacao ao servidor e , este por sua vez 
@@ -55,28 +55,43 @@ DELETE -> é usado para apagar dados do projeto .
 
 
 
-app.get("/api/cliente/",(req,res)=>
-{
+app.get("/api/cliente/", (req, res) => {
     Cliente.find(
-        (erro,dados)=>{
-            if (erro){
-                return res.status(400).send({output:`erro ao tentar ler os clientes -> ${erro}`});
+        (erro, dados) => {
+            if (erro) {
+                return res.status(400).send({ output: `erro ao tentar ler os clientes -> ${erro}` });
             }
-            res.status(200).send({output:dados});
+            res.status(200).send({ output: dados });
         }
     );
-}); 
+});
 
-app.post("/api/cliente/cadastro",(req,res)=>{
-    res.send(`Os dados enviados foram enviados ${req.body.nome}`);
-}); 
+app.post("/api/cliente/cadastro", (req, res) => {
+    const cliente = new Cliente(req.body);
+    cliente.save().then(() => {
+        res.status(201).send({ output: `Cliente cadastrado` })
+    })
+        .catch((erro) => res.status(400).send({ output: `erro ao tentar cadastrar o cliente -> ${erro}` }))
+});
 
-app.put("/api/cliente/atualizar/:id",(req,res)=>{
-    res.send(`o id passado foi ${req.params.id} e os dados para atualizar sao ${req.body}`); 
-}) 
 
-app.delete("/api/cliente/apagar/:id",(req,res) =>{
-    res.send(`O id pasado foi ${req.params.id}`);
-}) 
 
-app.listen(3000,()=>console.log("Servidor online em http://localhost:3000"));
+app.put("/api/cliente/atualizar/:id", (req, res) => {
+    Cliente.findByIdAndUpdate(req.params.id,req.body,(erro,dados) => { 
+        if(erro){
+            return res.status(400).send({output:`Erro ao tentar atualizar -> ${erro}`});
+        }
+        res.status(200.).send({output:`dados atualizados`});
+
+    })
+})
+
+app.delete("/api/cliente/apagar/:id", (req, res) => {
+    Cliente.findByIdAndDelete(req.params.id,(erro,dados)=>{
+        if(erro){
+            return res.status(204).send({});
+        };
+    })
+})
+
+app.listen(3000, () => console.log("Servidor online em http://localhost:3000"));
